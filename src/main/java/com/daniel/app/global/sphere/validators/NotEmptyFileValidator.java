96bc2b667298f5ea7350b4d0cdee0d8ec67673c4
LIBERTY-D <danielliberty2425@gmail.com>
2025-09-15 13:12:38 +0200

    aded custom validator fo my NotEmptyFile annotation

--- src/main/java/com/daniel/app/global/sphere/validators/NotEmptyFileValidator.java
+++ src/main/java/com/daniel/app/global/sphere/validators/NotEmptyFileValidator.java
@@ -1,2 +1,13 @@
-package com.daniel.app.global.sphere.validators;public class NotEmptyFileValidator {
+package com.daniel.app.global.sphere.validators;
+
+import com.daniel.app.global.sphere.annotation.NotEmptyFile;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.web.multipart.MultipartFile;
+
+public class NotEmptyFileValidator implements ConstraintValidator<NotEmptyFile, MultipartFile> {
+    @Override
+    public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
+        return file != null && !file.isEmpty();
+    }
 }
