6053aa633851b32836f344f6ff2e11291b485ca1
LIBERTY-D <danielliberty2425@gmail.com>
2025-09-18 16:58:54 +0200

    made sure i point the about page to the correct view path

--- src/main/java/com/daniel/app/global/sphere/config/WebConfig.java
+++ src/main/java/com/daniel/app/global/sphere/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
-        registry.addViewController("/about").setViewName("about");
+        registry.addViewController("/about").setViewName("pages/about/about");
 
     }
 }
